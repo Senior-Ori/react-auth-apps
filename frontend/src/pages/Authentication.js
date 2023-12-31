a09f51f0f0957ff1000eaadd25cedf9ae8053ae3
senior_Ori <ori8834@gmail.com>
2023-12-31 21:02:16 +0200

    Attaching Auth Tokens to Outgoing Requests
    Attaching Auth Tokens to Outgoing Requests

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -35,5 +35,10 @@ export async function action({ request }) {
   }
 
   // TODO: handle that token
+  const resData = await response.json();
+  const token = resData.token;
+
+  localStorage.setItem("token", token);
+
   return redirect("/");
 }
